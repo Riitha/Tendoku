38f5fbae3c90f0a13291a08f6cff118466389c25
Riitha <myuuta29.00@gmail.com>
2025-06-05 22:44:41 +0700

    fetching data

--- main.js
+++ main.js
@@ -6,5 +6,26 @@ function formatDate(date) {
         month: "long",
     });
 }
-let hasil = formatDate("2025-06-05")
-console.log(hasil);
+//async await
+const url = "https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&daily=weather_code,temperature_2m_min&timezone=Asia%2FTokyo"
+
+async function fetchingData() {
+    try {
+        const res = await fetch(url);
+        const forecast = await res.json();
+        const container = document.getElementById("card-container")
+
+        for (let i = 0; i < forecast.daily.time.length; i++) {
+            const tanggal = formatDate(forecast.daily.time[i]);
+            const suhu = forecast.daily.temperature_2m_min[i];
+
+            const card=`
+            <li><p>${tanggal}</p><p>最低:${suhu}</p></li>`;
+
+            container.innerHTML += card;
+        }
+    } catch(error) {
+        console.error("data tidak ditemukan", error)
+    }
+}
+fetchingData();
